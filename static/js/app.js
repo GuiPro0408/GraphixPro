c2b2f9b8c9d96ea29b073af26204f03dfd3d8725
Guillaume Juste <guillaume.juste0408@gmail.com>
2024-03-12 16:02:51 +0400

    Edit navbar to stay fix whens scroll

--- static/js/app.js
+++ static/js/app.js
@@ -6,22 +6,3 @@ $('.menu-toggle').click(function() {
   // add animation to open or close menu
   $('ul').css('animation', $(this).hasClass('active') ? 'open-menu-animation 0.5s forwards' : 'close-menu-animation 0.5s forwards');
 });
-
-// if rezise windows and is bigger than 550px, then remove the active class and animation
-$(window).resize(function() {
-  if ($(window).width() > 550) {
-    $('.menu-toggle').removeClass('active');
-    $('.main-nav').removeClass('active');
-    $('ul').css('animation', '');
-  }
-});
-
-// fix the menu when scroll with smooth transition
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 50) {
-    $('.site-header').addClass('fixed');
-  }
-  else {
-    $('.site-header').removeClass('fixed');
-  }
-});
