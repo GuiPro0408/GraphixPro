a3f4353de2331733498785232a2d14c9a1f8ab8e
Guillaume Juste <guillaume.juste0408@gmail.com>
2024-03-12 16:00:19 +0400

    Edit navbar to stay fix whens scroll

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,3 @@
-{/* <header class="site-header">
-      <a href="{% url 'index' %}" class="logo-container">
-        <img src="https://img.freepik.com/vecteurs-libre/symbole-anarchie-design-plat-dessine-main_23-2149244760.jpg?w=740&t=st=1710164909~exp=1710165509~hmac=7771938180154ad8ad3f17244ecac9c5036096a236c50036fbe41048afb89549" alt="Your Website Name" class="logo" />
-        <h1 id="website-name">Graphix Pro</h1>
-      </a>
-
-      <nav class="main-nav">
-        <div class="menu-toggle">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <ul>
-          <li>
-            <a href="{% url 'index' %}" class="nav-link">Home</a>
-          </li>
-          <li>
-            <a href="{% url 'about' %}" class="nav-link">About Us</a>
-          </li>
-          <li>
-            <a href="{% url 'services' %}" class="nav-link">Services</a>
-          </li>
-          <li>
-            <a href="{% url 'portfolio' %}" class="nav-link">Portfolio</a>
-          </li>
-          <li>
-            <a href="{% url 'contact' %}" class="nav-link">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header> */}
-
 // Toggling the menu
 $('.menu-toggle').click(function() {
   $(this).toggleClass('active');
@@ -45,5 +13,15 @@ $(window).resize(function() {
     $('.menu-toggle').removeClass('active');
     $('.main-nav').removeClass('active');
     $('ul').css('animation', '');
+  }
+});
+
+// fix the menu when scroll with smooth transition
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 50) {
+    $('.site-header').addClass('fixed');
+  }
+  else {
+    $('.site-header').removeClass('fixed');
   }
 });
